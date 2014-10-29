c50809e42dd5b49bc32285ec1536b8cfd6fd169b
sunny-g <sunny.gonna@gmail.com>
2014-10-29 12:07:02 -0700

    Finish functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,28 @@
 var makeStack = function(){
   var someInstance = {};
+  var size = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    someInstance[size] = value;
+    size++;
   };
 
   someInstance.pop = function(){
+    if (size) {
+      size--;
+      var pop = someInstance[size];
+      delete someInstance[size]
+      return pop;
+    }
   };
 
   someInstance.size = function(){
+    return size;
+
   };
 
   return someInstance;
